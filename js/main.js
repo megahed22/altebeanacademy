2215c5e931eb17101fcba621815a420f2f227ef1
megahed <altebeanacademy@gmail.com>
2025-01-15 17:28:31 -0500

    أضفت تصميم الصفحة الرئيسية مع تأثيرات الحركة

--- js/main.js
+++ js/main.js
@@ -1,27 +1,8 @@
-// main.js
-import { loginUser, registerUser } from './firebase-auth.js';
+// تأثير كتابة العنوان الرئيسى بشكل تدريجي
+window.addEventListener('DOMContentLoaded', (event) => {
+    const heroTitle = document.getElementById('hero-title');
+    const heroDescription = document.getElementById('hero-description');
 
-const loginForm = document.getElementById('loginForm');
-const registerForm = document.getElementById('registerForm');
-
-loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const email = loginForm.email.value;
-    const password = loginForm.password.value;
-    loginUser(email, password)
-        .then(() => {
-            window.location.href = '/teacher-dashboard.html'; // الانتقال إلى لوحة المعلم
-        })
-        .catch(error => alert(error.message));
-});
-
-registerForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const email = registerForm.email.value;
-    const password = registerForm.password.value;
-    registerUser(email, password)
-        .then(() => {
-            window.location.href = '/teacher-dashboard.html'; // الانتقال إلى لوحة المعلم
-        })
-        .catch(error => alert(error.message));
+    heroTitle.classList.add('fade-in');
+    heroDescription.classList.add('fade-in');
 });
